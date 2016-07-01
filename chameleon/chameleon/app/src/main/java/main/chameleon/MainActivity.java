c65114b79b48a8962626c90ca3679e351a548faf
Ksj7 <lobstar733@gmail.com>
2016-07-01 22:34:28 +0900

    Modify savedlist and Modelist
    Modify savedlist and Modelist

--- chameleon/chameleon/app/src/main/java/main/chameleon/MainActivity.java
+++ chameleon/chameleon/app/src/main/java/main/chameleon/MainActivity.java
@@ -1,194 +1,116 @@
 package main.chameleon;
 
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ScrollView;
 
-import com.nhaarman.listviewanimations.ArrayAdapter;
-import com.nhaarman.listviewanimations.appearance.simple.AlphaInAnimationAdapter;
-import com.nhaarman.listviewanimations.itemmanipulation.DynamicListView;
-import com.nhaarman.listviewanimations.itemmanipulation.dragdrop.OnItemMovedListener;
-import com.nhaarman.listviewanimations.itemmanipulation.dragdrop.TouchViewDraggableManager;
-import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.OnDismissCallback;
-import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.SimpleSwipeUndoAdapter;
-import com.nhaarman.listviewanimations.itemmanipulation.swipedismiss.undo.UndoAdapter;
-
-import java.util.Arrays;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
+    Button toSaveListButton;
+    Button toModeListButton;
+    Intent aIntent;
 
-    private static final int INITIAL_DELAY_MILLIS = 300;
-    private int mNewItemCount;
+    ListView listView;
+    ScrollView scrollView;
 
+    private ArrayList<Info> save_menu ;
+
+    static double longitude;
+    static double latitude;
+    static long starttime;
+    static long stoptime;
+
+    final String[] weekList = {"월","화","수","목","금","토","일"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DynamicListView listView = (DynamicListView) findViewById(R.id.activity_dynamiclistview_listview);
-        listView.addHeaderView(LayoutInflater.from(this).inflate(R.layout.activity_dynamiclistview_header, listView, false));
+        save_menu = new ArrayList<Info>();
+        scrollView = (ScrollView) findViewById(R.id.scroll_view);
+        listView = (ListView) findViewById(R.id.showModeListView);
+        save_menu.add(new Info(1, "서영학", 1, 1, 0, 1, 0, 1, weekList[1], 32, 32, 1234, 123, 1));
+        save_menu.add(new Info(1, "서영학", 0, 1, 0, 0, 0, 1, weekList[3], 32, 32, 1234, 123, 1));
+        save_menu.add(new Info(1, "서영학", 1, 1, 1, 1, 1, 1, weekList[3], 32, 32, 1234, 123, 1));
+        save_menu.add(new Info(1, "서영학", 1, 1, 1, 1, 1, 1, weekList[3], 32, 32, 1234, 123, 1));
+        save_menu.add(new Info(1, "서영학", 0, 0, 0, 0, 0, 0, weekList[3], 32, 32, 1234, 123, 1));
 
-        /* Setup the adapter */
-        ArrayAdapter<String> adapter = new MyListAdapter(this);
-        SimpleSwipeUndoAdapter simpleSwipeUndoAdapter = new SimpleSwipeUndoAdapter(adapter, this, new MyOnDismissCallback(adapter));
-        AlphaInAnimationAdapter animAdapter = new AlphaInAnimationAdapter(simpleSwipeUndoAdapter);
-        animAdapter.setAbsListView(listView);
-        assert animAdapter.getViewAnimator() != null;
-        animAdapter.getViewAnimator().setInitialDelayMillis(INITIAL_DELAY_MILLIS);
-        listView.setAdapter(animAdapter);
+        listView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                scrollView.requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+        });
 
-        /* Enable drag and drop functionality */
-        listView.enableDragAndDrop();
-        listView.setDraggableManager(new TouchViewDraggableManager(R.id.list_row_draganddrop_touchview));
-        listView.setOnItemMovedListener(new MyOnItemMovedListener(adapter));
-        listView.setOnItemLongClickListener(new MyOnItemLongClickListener(listView));
+        myAdapter adapter = new myAdapter();
+        listView.setAdapter(adapter);
 
-        /* Enable swipe to dismiss */
-        listView.enableSimpleSwipeUndo();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("position_save", position);
+                startActivity(intent);
+            }
+        });
 
-        /* Add new items on item click */
-        listView.setOnItemClickListener(new MyOnItemClickListener(listView));
+
+        toModeListButton = (Button) findViewById(R.id.toModeListButton);
+        toModeListButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                aIntent = new Intent(MainActivity.this, ModeActivity.class);
+                startActivity(aIntent);
+            }
+        });
+
+
     }
 
-    private static class MyListAdapter extends ArrayAdapter<String> implements UndoAdapter {
 
-        private final Context mContext;
 
-        MyListAdapter(final Context context) {
-            mContext = context;
-            for (int i = 0; i < 20; i++) {
-                add(mContext.getString(R.string.row_number, i));
-            }
-        }
+    private class myAdapter extends BaseAdapter {
 
         @Override
-        public long getItemId(final int position) {
-            return getItem(position).hashCode();
+        public int getCount() {
+            return save_menu.size();
         }
-
         @Override
-        public boolean hasStableIds() {
-            return true;
+        public Object getItem(int position) {
+            return save_menu.get(position);
         }
-
         @Override
-        public View getView(final int position, final View convertView, final ViewGroup parent) {
-            View view = convertView;
-            if (view == null) {
-                view = LayoutInflater.from(mContext).inflate(R.layout.list_row_dynamiclistview, parent, false);
-            }
+        public long getItemId(int position) {
+            return position;
+        }
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            EachSaveListView view = new EachSaveListView(getApplicationContext());
+            view.setListNameText(save_menu.get(position).getName());
+            view.setWeekNameText(save_menu.get(position).getWeek());
+            view.setStartNameText(save_menu.get(position).getStarttime() + "");
+            view.setStopNameText(save_menu.get(position).getStoptime() + "");
 
-            ((TextView) view.findViewById(R.id.list_row_draganddrop_textview)).setText(getItem(position));
+            view.setBrightFunction(save_menu.get(position).getBrightness());
+            view.setWifiFunction(save_menu.get(position).getWifi());
+            view.setBluetoothFunction(save_menu.get(position).getBluetooth());
+            view.setNetworkFunction(save_menu.get(position).getLte());
+            view.setAirplaneFunction(save_menu.get(position).getAirplane());
+            view.setSoundFunction(save_menu.get(position).getSound());
 
             return view;
-        }
+            //  public Info(int id, String name, int sound, int brightness, int wifi, int lte, int bluetooth, int airplane, String week, double latitude, double longitude, long starttime, long stoptime, int toggle) {
 
-        @NonNull
-        @Override
-        public View getUndoView(final int position, final View convertView, @NonNull final ViewGroup parent) {
-            View view = convertView;
-            if (view == null) {
-                view = LayoutInflater.from(mContext).inflate(R.layout.undo_row, parent, false);
-            }
-            return view;
-        }
-
-        @NonNull
-        @Override
-        public View getUndoClickView(@NonNull final View view) {
-            return view.findViewById(R.id.undo_row_undobutton);
-        }
-    }
-
-    private static class MyOnItemLongClickListener implements AdapterView.OnItemLongClickListener {
-
-        private final DynamicListView mListView;
-
-        MyOnItemLongClickListener(final DynamicListView listView) {
-            mListView = listView;
-        }
-
-        @Override
-        public boolean onItemLongClick(final AdapterView<?> parent, final View view, final int position, final long id) {
-            if (mListView != null) {
-                mListView.startDragging(position - mListView.getHeaderViewsCount());
-            }
-            return true;
-        }
-    }
-
-    private class MyOnDismissCallback implements OnDismissCallback {
-
-        private final ArrayAdapter<String> mAdapter;
-
-        @Nullable
-        private Toast mToast;
-
-        MyOnDismissCallback(final ArrayAdapter<String> adapter) {
-            mAdapter = adapter;
-        }
-
-        @Override
-        public void onDismiss(@NonNull final ViewGroup listView, @NonNull final int[] reverseSortedPositions) {
-            for (int position : reverseSortedPositions) {
-                mAdapter.remove(position);
-            }
-
-            if (mToast != null) {
-                mToast.cancel();
-            }
-            mToast = Toast.makeText(
-                    MainActivity.this,
-                    getString(R.string.removed_positions, Arrays.toString(reverseSortedPositions)),
-                    Toast.LENGTH_LONG
-            );
-            mToast.show();
-        }
-    }
-
-    private class MyOnItemMovedListener implements OnItemMovedListener {
-
-        private final ArrayAdapter<String> mAdapter;
-
-        private Toast mToast;
-
-        MyOnItemMovedListener(final ArrayAdapter<String> adapter) {
-            mAdapter = adapter;
-        }
-
-        @Override
-        public void onItemMoved(final int originalPosition, final int newPosition) {
-            if (mToast != null) {
-                mToast.cancel();
-            }
-
-            mToast = Toast.makeText(getApplicationContext(), getString(R.string.moved, mAdapter.getItem(newPosition), newPosition), Toast.LENGTH_SHORT);
-            mToast.show();
-        }
-    }
-
-    private class MyOnItemClickListener implements AdapterView.OnItemClickListener {
-
-        private final DynamicListView mListView;
-
-        MyOnItemClickListener(final DynamicListView listView) {
-            mListView = listView;
-        }
-
-        @Override
-        public void onItemClick(final AdapterView<?> parent, final View view, final int position, final long id) {
-            mListView.insert(position, getString(R.string.newly_added_item, mNewItemCount));
-            mNewItemCount++;
         }
     }
 }
-
