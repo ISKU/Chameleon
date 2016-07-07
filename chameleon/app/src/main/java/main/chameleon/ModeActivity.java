1c9ad63283e2b60fcfce3ae169c3946f378a7643
Ksj7 <lobstar733@gmail.com>
2016-07-08 02:00:43 +0900

    Redesign settingList file

--- chameleon/app/src/main/java/main/chameleon/ModeActivity.java
+++ chameleon/app/src/main/java/main/chameleon/ModeActivity.java
@@ -6,7 +6,6 @@ package main.chameleon;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
