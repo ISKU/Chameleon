d650234a5f0b5d175e9c759c9193b3ebb3a81814
Kim Min-Ho <minho1a@hanmail.net>
2016-07-30 18:13:11 +0900

    fix InfoTest Class, add UnitTest

--- app/src/test/java/main/chameleon/InfoTest.java
+++ app/src/test/java/main/chameleon/InfoTest.java
@@ -9,10 +9,53 @@ public class InfoTest {
     @Test
     public void testInfo() {
         Info info = new Info();
+        Info testinfo = new Info(1, "a", 1 , 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+
         assertEquals(4, 2 + 2);
         assertEquals(100, info.testInt());
 
+        info.setId(1);
+        assertEquals(1, info.getId());
+
+        info.setName("a");
+        assertEquals("a", info.getName());
+
+        info.setWeek(1);
+        assertEquals(1, info.getWeek());
+
+        info.setStarttime(1);
+        assertEquals(1, info.getStarttime());
+
+        info.setStoptime(1);
+        assertEquals(1, info.getStoptime());
+
+        info.setSound(1);
+        assertEquals(1, info.getSound());
+
+        info.setWifi(1);
+        assertEquals(1, info.getWifi());
+
+        info.setBrightness(1);
+        assertEquals(1, info.getBrightness());
+
+        info.setLte(1);
+        assertEquals(1, info.getLte());
+
+        info.setBluetooth(1);
+        assertEquals(1, info.getBluetooth());
+
+        info.setAirplane(1);
+        assertEquals(1, info.getAirplane());
+
+        info.setLatitude(1);
+        assertEquals(1, info.getLatitude());
+
+        info.setLongitude(1);
+        assertEquals(1, info.getLongitude());
+
         info.setToggle(1);
         assertEquals(1, info.getToggle());
+
+        assertEquals("a", info.toString());
     }
 }
